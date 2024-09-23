633d0b5a9d363ffb6ac255e0dfacb9caf6a4e311
artem hloba <artemgloba2@gmail.com>
2024-09-23 22:02:26 +0300

    Refactor with Prettier

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,199 +1,214 @@
-'use strict';
+'use strict'
 
-const WEATHER_URL = "https://api.open-meteo.com/v1/forecast?latitude=47.8517&longitude=35.1171&hourly=temperature_2m&daily=temperature_2m_max&current_weather=true&timezone=Europe/Kiev";
-const threeDaysWeatherUrl = "https://api.open-meteo.com/v1/forecast?latitude=47.8517&longitude=35.1171&daily=temperature_2m_max,precipitation_sum,wind_speed_10m_max&wind_speed_unit=ms&timezone=GMT&forecast_days=3";
-const sevenDaysWeatherUrl = "https://api.open-meteo.com/v1/forecast?latitude=47.875&longitude=35.125&daily=temperature_2m_max,precipitation_sum,wind_speed_10m_max&wind_speed_unit=ms&timezone=GMT&forecast_days=7";
+const WEATHER_URL =
+  'https://api.open-meteo.com/v1/forecast?latitude=47.8517&longitude=35.1171&hourly=temperature_2m&daily=temperature_2m_max&current_weather=true&timezone=Europe/Kiev'
+const threeDaysWeatherUrl =
+  'https://api.open-meteo.com/v1/forecast?latitude=47.8517&longitude=35.1171&daily=temperature_2m_max,precipitation_sum,wind_speed_10m_max&wind_speed_unit=ms&timezone=GMT&forecast_days=3'
+const sevenDaysWeatherUrl =
+  'https://api.open-meteo.com/v1/forecast?latitude=47.875&longitude=35.125&daily=temperature_2m_max,precipitation_sum,wind_speed_10m_max&wind_speed_unit=ms&timezone=GMT&forecast_days=7'
 
-let isCelsiDegree = true;
-let isKMToH = true;
-let threeDaysForecastData = null;
+let isCelsiDegree = true
+let isKMToH = true
+let threeDaysForecastData = null
 
-const tempUnitBtn = document.querySelector('.tempunitBtn');
-tempUnitBtn.textContent = `Переключитися на ${isCelsiDegree ? 'F' : 'C'}`;
+const tempUnitBtn = document.querySelector('.tempunitBtn')
+tempUnitBtn.textContent = `Переключитися на ${isCelsiDegree ? 'F' : 'C'}`
 
-const speedUnitBtn = document.querySelector('.speedBtn');
-speedUnitBtn.textContent = `Переключитися ${isKMToH ? 'ms' : 'km/h'}`;
+const speedUnitBtn = document.querySelector('.speedBtn')
+speedUnitBtn.textContent = `Переключитися ${isKMToH ? 'ms' : 'km/h'}`
 
-loadWeather(isKMToH, isCelsiDegree);
+loadWeather(isKMToH, isCelsiDegree)
 
-speedUnitBtn.onclick = switchSpeedUnit;
-tempUnitBtn.onclick = switchTemperatureUnit;
+speedUnitBtn.onclick = switchSpeedUnit
+tempUnitBtn.onclick = switchTemperatureUnit
 
-
-function switchSpeedUnit(){
-    isKMToH = !isKMToH;
-    speedUnitBtn.textContent = `Переключитися ${isKMToH ? 'ms' : 'km/h'}`;
-    loadWeather(isKMToH, isCelsiDegree);
+function switchSpeedUnit () {
+  isKMToH = !isKMToH
+  speedUnitBtn.textContent = `Переключитися ${isKMToH ? 'ms' : 'km/h'}`
+  loadWeather(isKMToH, isCelsiDegree)
 }
 
-function switchTemperatureUnit(){
-    isCelsiDegree = !isCelsiDegree;
-    tempUnitBtn.textContent = `Переключитися на ${isCelsiDegree ? 'F' : 'C'}`;
-    loadWeather(isKMToH, isCelsiDegree);
+function switchTemperatureUnit () {
+  isCelsiDegree = !isCelsiDegree
+  tempUnitBtn.textContent = `Переключитися на ${isCelsiDegree ? 'F' : 'C'}`
+  loadWeather(isKMToH, isCelsiDegree)
 }
 
-function loadWeather(isKMToH, isCelsiDegree){
-    fetch(`${WEATHER_URL}${isCelsiDegree ? '' : '&temperature_unit=fahrenheit'}${isKMToH ? '' : '&wind_speed_unit=ms'}`)
+function loadWeather (isKMToH, isCelsiDegree) {
+  fetch(
+    `${WEATHER_URL}${isCelsiDegree ? '' : '&temperature_unit=fahrenheit'}${
+      isKMToH ? '' : '&wind_speed_unit=ms'
+    }`
+  )
     .then(response => response.json())
     .then(data => generateWeather(data))
-    .catch(err => console.log(err));
+    .catch(err => console.log(err))
 }
 
 fetch(threeDaysWeatherUrl)
-    .then(response => response.json())
-    .then(data => {
-        threeDaysForecastData = data.daily;
-        generateThreeDaysWeather(data.daily);
-    })
-    .catch(err => console.log(err));
+  .then(response => response.json())
+  .then(data => {
+    threeDaysForecastData = data.daily
+    generateThreeDaysWeather(data.daily)
+  })
+  .catch(err => console.log(err))
 
-const showSevenDaysWeatherBtn = document.querySelector('.show-seven-days-weather-btn');
-showSevenDaysWeatherBtn.onclick = showSevenDaysWeather;
+const showSevenDaysWeatherBtn = document.querySelector(
+  '.show-seven-days-weather-btn'
+)
+showSevenDaysWeatherBtn.onclick = showSevenDaysWeather
 
-function showSevenDaysWeather() {
-    const sevenDaysMainContainer = document.querySelector('.seven-days-main-container');
-    if (sevenDaysMainContainer.style.display === "none" || !sevenDaysMainContainer.style.display) {
-        sevenDaysMainContainer.style.display = "block";
-        fetch(sevenDaysWeatherUrl)
-            .then(response => response.json())
-            .then(data => generateSevenDaysWeather(data.daily))
-            .catch(err => console.log(err));
-    } else {
-        sevenDaysMainContainer.style.display = "none";
-    }
+function showSevenDaysWeather () {
+  const sevenDaysMainContainer = document.querySelector(
+    '.seven-days-main-container'
+  )
+  if (
+    sevenDaysMainContainer.style.display === 'none' ||
+    !sevenDaysMainContainer.style.display
+  ) {
+    sevenDaysMainContainer.style.display = 'block'
+    fetch(sevenDaysWeatherUrl)
+      .then(response => response.json())
+      .then(data => generateSevenDaysWeather(data.daily))
+      .catch(err => console.log(err))
+  } else {
+    sevenDaysMainContainer.style.display = 'none'
+  }
 }
 
-function generateWeather({ 
-    current_weather: { temperature, windspeed }, 
-    current_weather_units: { temperature: tempUnit, windspeed: windUnit },
-    timezone 
+function generateWeather ({
+  current_weather: { temperature, windspeed },
+  current_weather_units: { temperature: tempUnit, windspeed: windUnit },
+  timezone
 }) {
-    
-    const currentTemperatureEl = document.querySelector('.temperatura');
-    const currentTimeZoneEl = document.querySelector('.location');
-    const currentWindspeedEl = document.querySelector('.speed');
-    const currentPrecipitationEl = document.querySelector('.precipitation-now');
-    const weatherImage = document.querySelector('.pic-about-weather');
+  const currentTemperatureEl = document.querySelector('.temperatura')
+  const currentTimeZoneEl = document.querySelector('.location')
+  const currentWindspeedEl = document.querySelector('.speed')
+  const currentPrecipitationEl = document.querySelector('.precipitation-now')
+  const weatherImage = document.querySelector('.pic-about-weather')
 
-    let colorFunc = isCelsiDegree ? calcTemperatureColor : calcTemperatureColorFahrenheit;
-    currentTemperatureEl.textContent = `Поточна температура: ${temperature} ${tempUnit}`;
-    currentTemperatureEl.style.color = colorFunc(temperature);
-    weatherImage.src = selectPic(temperature);
+  let colorFunc = isCelsiDegree
+    ? calcTemperatureColor
+    : calcTemperatureColorFahrenheit
+  currentTemperatureEl.textContent = `Поточна температура: ${temperature} ${tempUnit}`
+  currentTemperatureEl.style.color = colorFunc(temperature)
+  weatherImage.src = selectPic(temperature)
 
-    currentWindspeedEl.textContent = `Поточна швидкість вітру: ${windspeed} ${windUnit}`;
-    currentTimeZoneEl.textContent = `Місце розташування: ${timezone}`;
-    
-    if (threeDaysForecastData) {
-        currentPrecipitationEl.textContent = `Поточні опади: ${threeDaysForecastData.precipitation_sum[0]} мм`;
-    } else {
-        currentPrecipitationEl.textContent = `Поточні опади: дані недоступні`;
+  currentWindspeedEl.textContent = `Поточна швидкість вітру: ${windspeed} ${windUnit}`
+  currentTimeZoneEl.textContent = `Місце розташування: ${timezone}`
+
+  if (threeDaysForecastData) {
+    currentPrecipitationEl.textContent = `Поточні опади: ${threeDaysForecastData.precipitation_sum[0]} мм`
+  } else {
+    currentPrecipitationEl.textContent = `Поточні опади: дані недоступні`
+  }
+}
+
+function generateThreeDaysWeather (daily) {
+  const days = daily.time
+  const maxTemps = daily.temperature_2m_max
+  const precipitations = daily.precipitation_sum
+  const windSpeeds = daily.wind_speed_10m_max
+
+  const threeDaysWeatherCards = document.querySelectorAll(
+    '.threedays-weather-card'
+  )
+
+  threeDaysWeatherCards.forEach((card, index) => {
+    if (days[index]) {
+      const dayEl = card.querySelector('.day')
+      const maxTempEl = card.querySelector('.threedays-temperatura')
+      const precipitationEl = card.querySelector('.threedays-precipitation')
+      const windSpeedEl = card.querySelector('.threedays-speed')
+      const weatherImage = card.querySelector('.threedays-pic-about-weather')
+
+      dayEl.textContent = `Дата: ${days[index]}`
+      maxTempEl.textContent = `Температура: ${maxTemps[index]}°C`
+      precipitationEl.textContent = `Опади: ${precipitations[index]} мм`
+      windSpeedEl.textContent = `Швидкість вітру: ${windSpeeds[index]} m/s`
+
+      maxTempEl.style.color = calcTemperatureColor(maxTemps[index])
+      weatherImage.src = selectPic(maxTemps[index])
     }
+  })
 }
 
-function generateThreeDaysWeather(daily) {
-    const days = daily.time;
-    const maxTemps = daily.temperature_2m_max;
-    const precipitations = daily.precipitation_sum;
-    const windSpeeds = daily.wind_speed_10m_max;
+function generateSevenDaysWeather (daily) {
+  const days = daily.time
+  const maxTemps = daily.temperature_2m_max
+  const precipitations = daily.precipitation_sum
+  const windSpeeds = daily.wind_speed_10m_max
 
-    const threeDaysWeatherCards = document.querySelectorAll('.threedays-weather-card');
-    
-    threeDaysWeatherCards.forEach((card, index) => {
-        if (days[index]) {
-            const dayEl = card.querySelector('.day');
-            const maxTempEl = card.querySelector('.threedays-temperatura');
-            const precipitationEl = card.querySelector('.threedays-precipitation');
-            const windSpeedEl = card.querySelector('.threedays-speed');
-            const weatherImage = card.querySelector('.threedays-pic-about-weather');
+  const sevenDaysWeatherCards = document.querySelectorAll(
+    '.seven-days-weather-card'
+  )
 
-            dayEl.textContent = `Дата: ${days[index]}`;
-            maxTempEl.textContent = `Температура: ${maxTemps[index]}°C`;
-            precipitationEl.textContent = `Опади: ${precipitations[index]} мм`;
-            windSpeedEl.textContent = `Швидкість вітру: ${windSpeeds[index]} m/s`;
+  sevenDaysWeatherCards.forEach((card, index) => {
+    if (days[index]) {
+      const dayEl = card.querySelector('.seven-days-date')
+      const maxTempEl = card.querySelector('.seven-days-temperatura')
+      const precipitationEl = card.querySelector('.seven-days-precipitation')
+      const windSpeedEl = card.querySelector('.seven-days-speed')
+      const weatherImage = card.querySelector('.seven-days-pic-about-weather')
 
-            maxTempEl.style.color = calcTemperatureColor(maxTemps[index]);
-            weatherImage.src = selectPic(maxTemps[index]);
-        }
-    });
-}
+      dayEl.textContent = `Дата: ${days[index]}`
+      maxTempEl.textContent = `Температура: ${maxTemps[index]}°C`
+      precipitationEl.textContent = `Опади: ${precipitations[index]} mm`
+      windSpeedEl.textContent = `Швидкість вітру: ${windSpeeds[index]} m/s`
 
-function generateSevenDaysWeather(daily) {
-    const days = daily.time;
-    const maxTemps = daily.temperature_2m_max;
-    const precipitations = daily.precipitation_sum;
-    const windSpeeds = daily.wind_speed_10m_max;
-
-    const sevenDaysWeatherCards = document.querySelectorAll('.seven-days-weather-card');
-    
-    sevenDaysWeatherCards.forEach((card, index) => {
-        if (days[index]) {
-
-            const dayEl = card.querySelector('.seven-days-date');
-            const maxTempEl = card.querySelector('.seven-days-temperatura');
-            const precipitationEl = card.querySelector('.seven-days-precipitation');
-            const windSpeedEl = card.querySelector('.seven-days-speed');
-            const weatherImage = card.querySelector('.seven-days-pic-about-weather');
-
-            dayEl.textContent = `Дата: ${days[index]}`;
-            maxTempEl.textContent = `Температура: ${maxTemps[index]}°C`;
-            precipitationEl.textContent = `Опади: ${precipitations[index]} mm`;
-            windSpeedEl.textContent = `Швидкість вітру: ${windSpeeds[index]} m/s`;
-
-            maxTempEl.style.color = calcTemperatureColor(maxTemps[index]);
-            weatherImage.src = selectPic(maxTemps[index]);
-        }
-    });
-}
-
-function updateRealTime() {
-    const realTime = document.querySelector('.real-time');
-    const now = new Date();
-    const dateOptions = { 
-        weekday: 'long', 
-        year: 'numeric', 
-        month: 'long', 
-        day: 'numeric' 
-    };
-    const dateStr = now.toLocaleDateString('uk-UA', dateOptions);
-    realTime.textContent = `${dateStr}`;
-}
-
-function calcTemperatureColorFahrenheit(temperature) {
-    switch (true) {
-        case (temperature <= 32):
-            return 'blue';
-        case (temperature <= 68):
-            return 'green';
-        case (temperature <= 86):
-            return 'orange';
-        default:
-            return 'red';
+      maxTempEl.style.color = calcTemperatureColor(maxTemps[index])
+      weatherImage.src = selectPic(maxTemps[index])
     }
+  })
 }
 
-
-function calcTemperatureColor(temperature) {
-    switch (true) {
-        case (temperature <= 0):
-            return 'blue';
-        case (temperature <= 20):
-            return 'green';
-        case (temperature <= 30):
-            return 'orange';
-        default:
-            return 'red';
-    }
+function updateRealTime () {
+  const realTime = document.querySelector('.real-time')
+  const now = new Date()
+  const dateOptions = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  }
+  const dateStr = now.toLocaleDateString('uk-UA', dateOptions)
+  realTime.textContent = `${dateStr}`
 }
 
-
-function selectPic(temperature) {
-    if (temperature < 0) {
-        return "./assets/img/icon-snowy.png";
-    } else if (temperature <= 20) {
-        return "./assets/img/icon-sun.png";
-    } else {
-        return "./assets/img/icon-sun.png";
-    }
+function calcTemperatureColorFahrenheit (temperature) {
+  switch (true) {
+    case temperature <= 32:
+      return 'blue'
+    case temperature <= 68:
+      return 'green'
+    case temperature <= 86:
+      return 'orange'
+    default:
+      return 'red'
+  }
 }
 
-updateRealTime();
+function calcTemperatureColor (temperature) {
+  switch (true) {
+    case temperature <= 0:
+      return 'blue'
+    case temperature <= 20:
+      return 'green'
+    case temperature <= 30:
+      return 'orange'
+    default:
+      return 'red'
+  }
+}
+
+function selectPic (temperature) {
+  if (temperature < 0) {
+    return './assets/img/icon-snowy.png'
+  } else if (temperature <= 20) {
+    return './assets/img/icon-sun.png'
+  } else {
+    return './assets/img/icon-sun.png'
+  }
+}
+
+updateRealTime()
--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,44 +1,42 @@
 const slides = [
-    {
-        src: './assets/img/zp-img-3.jpg',
-        alt: 'landscape1',
-    },
-    {
-        src: './assets/img/zp-img-2.jpg',
-        alt: 'landscape2',
-    },
-    {
-        src: './assets/img/zp-img-1.jpg',
-        alt: 'landscape3',
-    },
-    {
-        src: './assets/img/zp-img-4.jpg',
-        alt: 'landscape4',
-    },
-    ];
-    
-    let currentSlideIndex = 0;
-    
-    
-    const sliderImg = document.querySelector(".sliderImg")
-    updatesSlider(currentSlideIndex);
-    
-    const [prevBnt, nextbtn] = document.querySelectorAll('button')
-    
-    prevBnt.onclick = () => {
-        currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
-    
-        updatesSlider(currentSlideIndex);
-    };
-    
-    nextbtn.onclick = () => {
-        currentSlideIndex = (currentSlideIndex + 1) % slides.length;
-    
-        updatesSlider(currentSlideIndex);
-    };
-    
-    function updatesSlider(){
-        sliderImg.src = slides[currentSlideIndex].src;
-        sliderImg.alt = slides[currentSlideIndex].alt;   
-    }
-    
+  {
+    src: './assets/img/zp-img-3.jpg',
+    alt: 'landscape1'
+  },
+  {
+    src: './assets/img/zp-img-2.jpg',
+    alt: 'landscape2'
+  },
+  {
+    src: './assets/img/zp-img-1.jpg',
+    alt: 'landscape3'
+  },
+  {
+    src: './assets/img/zp-img-4.jpg',
+    alt: 'landscape4'
+  }
+]
+
+let currentSlideIndex = 0
+
+const sliderImg = document.querySelector('.sliderImg')
+updatesSlider(currentSlideIndex)
+
+const [prevBnt, nextbtn] = document.querySelectorAll('button')
+
+prevBnt.onclick = () => {
+  currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length
+
+  updatesSlider(currentSlideIndex)
+}
+
+nextbtn.onclick = () => {
+  currentSlideIndex = (currentSlideIndex + 1) % slides.length
+
+  updatesSlider(currentSlideIndex)
+}
+
+function updatesSlider () {
+  sliderImg.src = slides[currentSlideIndex].src
+  sliderImg.alt = slides[currentSlideIndex].alt
+}
